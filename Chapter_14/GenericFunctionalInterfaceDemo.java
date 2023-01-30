3f65bb8290ca17c11492035a716c1fd60bc95ef3
9Sleepy9 <ushakov.a.i.1999@mail.ru>
2023-01-30 17:31:27 +0300

    Added console read

--- Chapter_14/GenericFunctionalInterfaceDemo.java
+++ Chapter_14/GenericFunctionalInterfaceDemo.java
@@ -1,7 +1,9 @@
 package Chapter_14;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 // Here we created Generic Functional Interface to test it with lambda
 interface SomeTest<T> {
@@ -12,17 +14,34 @@ public class GenericFunctionalInterfaceDemo {
 
     public static void main(String[] args) throws IOException {
 
+        int divisibleInt, dividerInt;
+        // Creating BufferedReader object to read numbers from console
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str; // Object to contain String read from console
+
         // Testing some lambda functions with different types
         SomeTest<Integer> isIntFactor = (n, m) -> (n % m) == 0;
 
         System.out.println("Checking isIntFactor lambda: ");
-        int divisibleInt = 100;
-        int dividerInt = 10;
+        do {
+            try {
+                System.out.println("Give me divisible int number: ");
+                str = br.readLine();
+                divisibleInt = Integer.parseInt(str);
+                System.out.println("Give me divider int number: ");
+                str = br.readLine();
+                dividerInt = Integer.parseInt(str);
 
-        if (isIntFactor.test(divisibleInt, dividerInt))
-            System.out.println("Number " + dividerInt + " is factor for " + divisibleInt);
-        else
-            System.out.println("Number " + dividerInt + " is NOT factor for " + divisibleInt);
+                if (isIntFactor.test(divisibleInt, dividerInt))
+                    System.out.println("Number " + dividerInt + " is factor for " + divisibleInt);
+                else
+                    System.out.println("Number " + dividerInt + " is NOT factor for " + divisibleInt);
+
+                break;
+            } catch (NumberFormatException exc) {
+                System.out.println("\nPlease give me correct number...\n");
+            }
+        } while (true);
 
         System.out.println();
 
@@ -40,10 +59,10 @@ public class GenericFunctionalInterfaceDemo {
 
         SomeTest<String> isIn = (n, m) -> n.contains(m);
 
-        String str = "Generic Functional Interface";
+        String strTest = "Generic Functional Interface";
 
-        System.out.println("Testing String: '" + str + "'");
-        if (isIn.test(str, "face"))
+        System.out.println("Testing String: '" + strTest + "'");
+        if (isIn.test(strTest, "face"))
             System.out.println("String contains 'face'");
         else
             System.out.println("String does not contains 'face'");
