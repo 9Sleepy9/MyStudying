653d604c5d85e0d02cd597b71293c433e2ae0406
9Sleepy9 <ushakov.a.i.1999@mail.ru>
2023-02-07 23:28:34 +0300

    ArrayList Simple Demo

---
+++ CollectionsDemo/ArrayListSimpleDemo.java
@@ -0,0 +1,53 @@
+package CollectionsDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Animal {
+    public void eat() {
+        System.out.println("Animal is eating...");
+    }
+}
+
+class Dog extends Animal {
+
+    public void eat() {
+        System.out.println("Dog is eating...");
+    }
+}
+
+class Cat extends Animal {
+
+    public void eat() {
+        System.out.println("Cat is eating...");
+    }
+}
+
+
+public class ArrayListSimpleDemo {
+
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        myList.add("str1");
+        myList.add("str2");
+        myList.add("str3");
+
+        for (String str : myList) {
+            System.out.println(str);
+        }
+
+        System.out.println();
+
+        List<Animal> animalList = new ArrayList<>();
+        animalList.add(new Dog());
+        animalList.add(new Cat());
+        animalList.add(new Animal());
+        testAnimal(animalList);
+    }
+
+    public static void testAnimal(List<? extends Animal> animals) {
+        for (Animal animal : animals) {
+            animal.eat();
+        }
+    }
+}
