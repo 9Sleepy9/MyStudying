e3998156bdabbeacab737236001fc6980a773101
9Sleepy9 <ushakov.a.i.1999@mail.ru>
2023-02-17 15:48:09 +0300

    Some Collections Algorithms Demo

---
+++ CollectionsDemo/AlgorithmsDemo.java
@@ -0,0 +1,37 @@
+package CollectionsDemo;
+
+import java.util.*;
+
+public class AlgorithmsDemo {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(-8);
+        ll.add(20);
+        ll.add(-20);
+        ll.add(8);
+
+        Comparator<Integer> r = Comparator.reverseOrder();
+
+        Collections.sort(ll, r);
+
+        System.out.print("Список отсортирован в обратном порядке: ");
+        for (int i : ll)
+            System.out.print(i + " ");
+
+        System.out.println();
+
+        Collections.shuffle(ll);
+
+        System.out.print("Список перетасован: ");
+
+        for (int i : ll)
+            System.out.print(i + " ");
+
+        System.out.println();
+
+        System.out.println("Минимум: " + Collections.min(ll));
+        System.out.println("Максимум: " + Collections.max(ll));
+    }
+}
